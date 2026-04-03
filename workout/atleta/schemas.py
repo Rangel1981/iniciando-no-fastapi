92fd14414a7e64912ca26ab8411cca64f5fcf880
Arthur <aarthur.rangel98@gamil.com>
2026-04-03 12:33:21 -0300

    retornar no models da pasta atletas do projeto com o FastAPI

---
+++ workout/atleta/schemas.py
@@ -0,0 +1,11 @@
+from typing import Annotated
+
+from pydantic import BaseModel, Field, PositiveFloat
+
+class Atleta(BaseModel):
+    nome: Annotated[str, Field(description="Nome do Atleta", examples="Arthur", max_length=50)]
+    cpf: Annotated[int, Field(description="CPF do Atleta", examples="00000000000", max_length=11)]
+    idade: Annotated[int, Field(description="Idade do Atleta", examples="28")]
+    peso: Annotated[PositiveFloat, Field(description="Peso do Atleta", examples=85.5)]
+    altura: Annotated[float, Field(description="Altura do Atleta", examples="180.5")]
+    sexo: Annotated[int, Field(description="Sexo do Atleta", examples="M", max_length=1)]
